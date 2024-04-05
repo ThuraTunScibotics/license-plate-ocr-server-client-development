e17c67a5e9522c139763dd947990c9671e2d7932
thuratun-gwm98 <thuratun@globalwalkers.co.jp>
2024-04-05 16:59:07 +0630

    Add: add init

---
+++ src/licenseplate_ocr/ocr_server/__init__.py
@@ -0,0 +1,8 @@
+"""License Plate OCR Project"""
+
+# from importlib import metadata
+
+# meta = metadata.metadata(__package__ or __name__)
+
+# __libname__ = meta["version"]
+# from ..ocr_engine.yolox.exp import *
